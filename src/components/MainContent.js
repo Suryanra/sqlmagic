78015ce777d12f642776c84731c946e727abc924
suryanra <suryapratapnra@gmail.com>
2025-03-26 22:28:30 +0530

    bug fix 1

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './MainContent.css'
-import TextEditor from './TextEditor'
+// import TextEditor from './TextEditor'
 const MainContent = () => {
   return (
     <div className='main-content'>
-      <TextEditor/>
+      {/* <TextEditor/> */}
     </div>
   )
 }
